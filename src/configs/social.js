6d75af4fcef56fa1f7d945f95f59cd3008c825c3
amir ehsan haji hussaini <niamileo@gmail.com>
2022-11-25 14:05:02 +0330

    social added to configs

---
+++ src/configs/social.js
@@ -0,0 +1,18 @@
+export default {
+  instagram: {
+    link: "ehsandevs",
+    visible: true,
+  },
+  linkedin: {
+    link: "amir-ehsan-haji-hussaini-4843b4112",
+    visible: true,
+  },
+  github: {
+    link: "niamileo",
+    visible: true,
+  },
+  twitter: {
+    link: "",
+    visible: false,
+  },
+};
